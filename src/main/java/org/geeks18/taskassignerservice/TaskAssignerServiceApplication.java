ee85b784b1d9d7da9a4ed562f85f6f06ce9aa0ec
Joydip Kumar <joydipkumar@Joydips-MacBook-Pro.local>
2021-02-27 22:51:30 +0000

    project files

---
+++ src/main/java/org/geeks18/taskassignerservice/TaskAssignerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.geeks18.taskassignerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAssignerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskAssignerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/geeks18/taskassignerservice/TaskAssignerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.geeks18.taskassignerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskAssignerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
